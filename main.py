16ddfe70a30664c6eff70cd298053f268055628a
wataru <92125110+wtaru@ users.noreply.github.com>
2024-11-17 21:49:10 +0900

    update script add

--- main.py
+++ main.py
@@ -47,6 +47,13 @@ def get_sales(user_id: int, db: Session=Depends(get_db)):
     sales = crud.get_sales(db=db, user_id=user_id)
     return sales
 
+@app.put("/users/{device_id}")
+def update_active(device_id: str, is_active: bool, db: Session = Depends(get_db)):
+    user = crud.update_user_active(db=db, device_id=device_id, is_active=is_active)
+    if user:
+        return {"message": "User authentication status updated successfully.", "user": user}
+    return {"message": "User not found."}, 404
+
 @app.delete("/users/{user_id}", response_model=schemas.MessageResponse)
 def deleteUser(user_id: int, db: Session=Depends(get_db)):
     success = crud.delete_user(db=db, user_id=user_id)
--- sql_app/crud.py
+++ sql_app/crud.py
@@ -48,10 +48,19 @@ def create_sales(db: Session, sales: schemas.SalesCreate, user_id: int):
     db.refresh(db_sales)
     return db_sales
 
+def update_user_active(db: Session, device_id: str, is_active: bool):
+    user = db.query(models.User).filter(models.User.device_id == device_id).first()
+    if user:
+        user.is_active = is_active
+        db.commit()
+        db.refresh(user)
+        return user
+    return None  # ユーザーが見つからなかった場合
+
 def delete_user(db: Session, user_id: int):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user:
         db.delete(user)
         db.commit()
         return True  # 削除が成功した場合は True を返す
-    return False  # ユーザーが見つからなかった場合は False を返す
+    return False
